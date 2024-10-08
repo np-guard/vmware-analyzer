14bbc18045395e5d779a4cffae97c9348dc2a262
haim <haim.kermany@gmail.com>
2024-10-08 18:08:04 +0300

    rearange code

---
+++ pkg/common/dotGraph.go
@@ -0,0 +1,69 @@
+/*
+Copyright 2023- IBM Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package common
+import (
+	"fmt"
+	"maps"
+	"slices"
+	"strings"
+
+)
+
+type node interface {
+	Name() string
+}
+type nodeID int
+type dotNode struct {
+	node
+	ID nodeID
+}
+
+func (n *dotNode) string() string {
+	return fmt.Sprintf("node_%d_[shape=box, label=%q, tooltip=%q]", n.ID, n.Name(), n.Name())
+}
+
+type dotEdge struct {
+	src, dst dotNode
+	label    string
+}
+
+func (e *dotEdge) string() string {
+	return fmt.Sprintf("node_%d_ -> node_%d_[label=%q, tooltip=%q labeltooltip=%q]",
+		e.src.ID, e.dst.ID, e.label, e.label, e.label)
+}
+
+type DotGraph struct {
+	edges         []dotEdge
+	nodeIDcounter nodeID
+	nodes         map[node]dotNode
+}
+
+func NewDotGraph() *DotGraph {
+	return &DotGraph{nodes: map[node]dotNode{}}
+}
+
+func (dotGraph *DotGraph) AddEdge(src, dst node, label string) {
+	for _, n := range []node{src, dst} {
+		if _, ok := dotGraph.nodes[n]; !ok {
+			dotGraph.nodes[n] = dotNode{n, dotGraph.nodeIDcounter}
+			dotGraph.nodeIDcounter++
+		}
+	}
+	dotGraph.edges = append(dotGraph.edges, dotEdge{dotGraph.nodes[src], dotGraph.nodes[dst], label})
+}
+
+func (dotGraph *DotGraph) String() string {
+	nodeLines := make([]string, len(dotGraph.nodes))
+	for i, n := range slices.Collect(maps.Values(dotGraph.nodes)) {
+		nodeLines[i] = n.string()
+	}
+	edgeLines := make([]string, len(dotGraph.edges))
+	for i, e := range dotGraph.edges {
+		edgeLines[i] = e.string()
+	}
+	return fmt.Sprintf("digraph{\n%s\n\n%s\n}\n", strings.Join(nodeLines, "\n"), strings.Join(edgeLines, "\n"))
+}
--- pkg/model/output.go
+++ pkg/model/output.go
@@ -1,11 +1,6 @@
 package model
 
 import (
-	"fmt"
-	"maps"
-	"slices"
-	"strings"
-
 	"github.com/np-guard/vmware-analyzer/pkg/common"
 )
 
@@ -27,7 +22,7 @@ func (c *config) output(params OutputParameters) (res string, err error) {
 	case TextFormat:
 		res = filteredConn.String()
 	case DotFormat:
-		res = createDotGraph(filteredConn.toSlice()).string()
+		res = createDotGraph(filteredConn.toSlice()).String()
 	}
 	if params.FileName != "" {
 		err := common.WriteToFile(params.FileName, res)
@@ -38,59 +33,10 @@ func (c *config) output(params OutputParameters) (res string, err error) {
 	return res, nil
 }
 
-// ///////////////////////////////////////////////////////////////////
-type node interface {
-	Name() string
-}
-type nodeID int
-type dotNode struct {
-	node
-	ID nodeID
-}
-
-func (n *dotNode) string() string {
-	return fmt.Sprintf("node_%d_[shape=box, label=%q, tooltip=%q]", n.ID, n.Name(), n.Name())
-}
-
-type dotEdge struct {
-	src, dst dotNode
-	label    string
-}
-
-func (e *dotEdge) string() string {
-	return fmt.Sprintf("node_%d_ -> node_%d_[label=%q, tooltip=%q labeltooltip=%q]",
-		e.src.ID, e.dst.ID, e.label, e.label, e.label)
-}
-
-type dotGraph struct {
-	nodes []dotNode
-	edges []dotEdge
-}
-
-func createDotGraph(conns []connMapEntry) *dotGraph {
-	nodes := map[node]dotNode{}
-	dotEdges := make([]dotEdge, len(conns))
-	var nodeIDcounter nodeID
-	for i, e := range conns {
-		for _, n := range []node{e.src, e.dst} {
-			if _, ok := nodes[n]; !ok {
-				nodes[n] = dotNode{n, nodeIDcounter}
-				nodeIDcounter++
-			}
-		}
-		dotEdges[i] = dotEdge{nodes[e.src], nodes[e.dst], e.conn.String()}
+func createDotGraph(conns []connMapEntry) *common.DotGraph {
+	g := common.NewDotGraph()
+	for _, e := range conns {
+		g.AddEdge(e.src, e.dst, e.conn.String())
 	}
-	return &dotGraph{slices.Collect(maps.Values(nodes)), dotEdges}
-}
-
-func (dotGraph *dotGraph) string() string {
-	nodeLines := make([]string, len(dotGraph.nodes))
-	for i, n := range dotGraph.nodes {
-		nodeLines[i] = n.string()
-	}
-	edgeLines := make([]string, len(dotGraph.edges))
-	for i, e := range dotGraph.edges {
-		edgeLines[i] = e.string()
-	}
-	return fmt.Sprintf("digraph{\n%s\n\n%s\n}\n", strings.Join(nodeLines, "\n"), strings.Join(edgeLines, "\n"))
+	return g
 }
