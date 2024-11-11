bdcb71d6778dbbb43430340fc6590615c726107a
shirim <shirim@il.ibm.com>
2024-11-11 12:07:19 +0200

    commenting due to private file no in repo

--- pkg/model/parser_test.go
+++ pkg/model/parser_test.go
@@ -1,25 +1,20 @@
 package model
 
-import (
-	"fmt"
-	"testing"
-)
-
-func TestParser(t *testing.T) {
-	parser, err := NewNSXConfigParserFromFile("../../docs/simple7.json")
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	err = parser.RunParser()
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-	config := parser.GetConfig()
-	fmt.Println(config.getConfigInfoStr())
-
-	config.ComputeConnectivity()
-	fmt.Println("analyzed Connectivity")
-	fmt.Println(config.analyzedConnectivity.String())
-	fmt.Println("done")
-}
+//func TestParser(t *testing.T) {
+//	parser, err := NewNSXConfigParserFromFile("../../docs/simple7.json")
+//	if err != nil {
+//		t.Fatal(err.Error())
+//	}
+//
+//	err = parser.RunParser()
+//	if err != nil {
+//		t.Fatal(err.Error())
+//	}
+//	config := parser.GetConfig()
+//	fmt.Println(config.getConfigInfoStr())
+//
+//	config.ComputeConnectivity()
+//	fmt.Println("analyzed Connectivity")
+//	fmt.Println(config.analyzedConnectivity.String())
+//	fmt.Println("done")
+//}
