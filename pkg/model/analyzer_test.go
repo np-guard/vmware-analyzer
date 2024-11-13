778abf13ed75269192105f144f78643a59dc4652
shirim <shirim@il.ibm.com>
2024-11-13 11:31:18 +0200

    lint

--- pkg/model/analyzer_test.go
+++ pkg/model/analyzer_test.go
@@ -1,94 +1,95 @@
 package model
 
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"testing"
-
-	"github.com/stretchr/testify/require"
-
-	"github.com/np-guard/vmware-analyzer/pkg/collector/data"
-	"github.com/np-guard/vmware-analyzer/pkg/internal/projectpath"
-)
-
-type analyzerTest struct {
-	name   string
-	exData data.Example
-}
-
-var allTests = []analyzerTest{
-	{
-		name:   "Example1",
-		exData: data.Example1,
-	},
-	{
-		name:   "Example2",
-		exData: data.Example2,
-	},
-}
-
-func (a *analyzerTest) file() string {
-	return a.name + ".txt"
-}
-
-func (a *analyzerTest) run(t *testing.T) {
-	var override bool
-	//nolint:gocritic // comment here should stay
-	//override = true // uncommnet to override expected output
-	rc := data.ExamplesGeneration(a.exData)
-	rcJSON, err := rc.ToJSONString()
-	require.Nil(t, err)
-	jsonPath := getJSONTestPath(a.name)
-	err = os.WriteFile(jsonPath, []byte(rcJSON), 0o600)
-	require.Nil(t, err)
-
-	require.Nil(t, err)
-
-	params := OutputParameters{
-		Format: "txt",
-	}
-	res, err := NSXConnectivityFromResourcesContainer(rc, params)
-	require.Nil(t, err)
-	fmt.Println(res)
-
-	expectedFile := getExpectedTestPath(a.file())
-	if override {
-		err := os.WriteFile(expectedFile, []byte(res), 0o600)
-		require.Nil(t, err)
-	} else {
-		// compare expected with actual output
-		expected, err := os.ReadFile(expectedFile)
-		expectedStr := string(expected)
-		require.Nil(t, err)
-		if expectedStr != res {
-			// gen actual output to enable manual diff after test run
-			actual := getActualTestPath(a.file())
-			err := os.WriteFile(actual, []byte(res), 0o600)
-			require.Nil(t, err)
-		}
-		require.Equal(t, expectedStr, res)
-	}
-	fmt.Println("done")
-}
-
-// todo: temp since the test fails
-//func TestAnalyzer(t *testing.T) {
-//	logging.Init(logging.HighVerbosity)
-//	for i := range allTests {
-//		test := &allTests[i]
-//		test.run(t)
-//	}
+//
+//import (
+//	"fmt"
+//	"os"
+//	"path/filepath"
+//	"testing"
+//
+//	"github.com/stretchr/testify/require"
+//
+//	"github.com/np-guard/vmware-analyzer/pkg/collector/data"
+//	"github.com/np-guard/vmware-analyzer/pkg/internal/projectpath"
+//)
+//
+//type analyzerTest struct {
+//	name   string
+//	exData data.Example
 //}
-
-func getExpectedTestPath(name string) string {
-	return filepath.Join(projectpath.Root, "pkg", "collector", "data", "expected_output", name)
-}
-
-func getActualTestPath(name string) string {
-	return filepath.Join(projectpath.Root, "pkg", "collector", "data", "actual_output", name)
-}
-
-func getJSONTestPath(name string) string {
-	return filepath.Join(projectpath.Root, "pkg", "collector", "data", "json", name+".json")
-}
+//
+//var allTests = []analyzerTest{
+//	{
+//		name:   "Example1",
+//		exData: data.Example1,
+//	},
+//	{
+//		name:   "Example2",
+//		exData: data.Example2,
+//	},
+//}
+//
+//func (a *analyzerTest) file() string {
+//	return a.name + ".txt"
+//}
+//
+//func (a *analyzerTest) run(t *testing.T) {
+//	var override bool
+//	//nolint:gocritic // comment here should stay
+//	//override = true // uncommnet to override expected output
+//	rc := data.ExamplesGeneration(a.exData)
+//	rcJSON, err := rc.ToJSONString()
+//	require.Nil(t, err)
+//	jsonPath := getJSONTestPath(a.name)
+//	err = os.WriteFile(jsonPath, []byte(rcJSON), 0o600)
+//	require.Nil(t, err)
+//
+//	require.Nil(t, err)
+//
+//	params := OutputParameters{
+//		Format: "txt",
+//	}
+//	res, err := NSXConnectivityFromResourcesContainer(rc, params)
+//	require.Nil(t, err)
+//	fmt.Println(res)
+//
+//	expectedFile := getExpectedTestPath(a.file())
+//	if override {
+//		err := os.WriteFile(expectedFile, []byte(res), 0o600)
+//		require.Nil(t, err)
+//	} else {
+//		// compare expected with actual output
+//		expected, err := os.ReadFile(expectedFile)
+//		expectedStr := string(expected)
+//		require.Nil(t, err)
+//		if expectedStr != res {
+//			// gen actual output to enable manual diff after test run
+//			actual := getActualTestPath(a.file())
+//			err := os.WriteFile(actual, []byte(res), 0o600)
+//			require.Nil(t, err)
+//		}
+//		require.Equal(t, expectedStr, res)
+//	}
+//	fmt.Println("done")
+//}
+//
+//// todo: temp since the test fails
+////func TestAnalyzer(t *testing.T) {
+////	logging.Init(logging.HighVerbosity)
+////	for i := range allTests {
+////		test := &allTests[i]
+////		test.run(t)
+////	}
+////}
+//
+//func getExpectedTestPath(name string) string {
+//	return filepath.Join(projectpath.Root, "pkg", "collector", "data", "expected_output", name)
+//}
+//
+//func getActualTestPath(name string) string {
+//	return filepath.Join(projectpath.Root, "pkg", "collector", "data", "actual_output", name)
+//}
+//
+//func getJSONTestPath(name string) string {
+//	return filepath.Join(projectpath.Root, "pkg", "collector", "data", "json", name+".json")
+//}
