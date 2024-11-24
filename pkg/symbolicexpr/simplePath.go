80a88d27aee33c207ae008a0569031ba4816917d
shirim <shirim@il.ibm.com>
2024-11-24 10:11:56 +0200

    added func (path *simplePath) negate() and tests to simplePath functionality

--- pkg/symbolicexpr/simplePath.go
+++ pkg/symbolicexpr/simplePath.go
@@ -3,13 +3,20 @@ package symbolicexpr
 import "strings"
 
 func (path *simplePath) string() string {
-	return path.src.string() + " -> " + path.dst.string()
+	return path.src.string() + " to " + path.dst.string()
 }
 
 func (paths *simplePaths) string() string {
+	if len(*paths) == 0 {
+		return ""
+	}
 	res := make([]string, len(*paths))
 	for i, path := range *paths {
 		res[i] = path.string()
 	}
 	return strings.Join(res, "\n")
+}
+
+func (path *simplePath) negate() *simplePath {
+	return &simplePath{src: path.src.negate(), dst: path.dst.negate()}
 }
--- pkg/symbolicexpr/symbolicexpr_test.go
+++ pkg/symbolicexpr/symbolicexpr_test.go
@@ -2,6 +2,7 @@ package symbolicexpr
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -17,7 +18,32 @@ func (testT *testTag) Name() string {
 	return testT.name
 }
 
-func TestSymbolicExpr(t *testing.T) {
+func TestSimplePaths(t *testing.T) {
+	simplePaths := simplePaths{}
+	for i := 1; i <= 5; i++ {
+		testTag1 := initTestTag(fmt.Sprintf("src-%v", i))
+		atomicSrc := &Atomic{label: testTag1, toVal: fmt.Sprintf("str1-%v", i), neg: i%2 == 0}
+		testTag2 := initTestTag(fmt.Sprintf("dst-%v", i))
+		atomicDst := &Atomic{label: testTag2, toVal: fmt.Sprintf("str2-%v", i), neg: i%2 == 0}
+		simplePaths = append(simplePaths, &simplePath{atomicSrc, atomicDst})
+	}
+	fmt.Printf("\nsimple paths:\n%v\n", simplePaths.string())
+	require.Contains(t, simplePaths.string(), "src-1 = str1-1 to dst-1 = str2-1",
+		"simple path0 not as expected")
+	require.Contains(t, simplePaths.string(), "src-2 != str1-2 to dst-2 != str2-2",
+		"simple path1 not as expected")
+	require.Contains(t, simplePaths.string(), "src-4 != str1-4 to dst-4 != str2-4",
+		"simple path3 not as expected")
+	require.Contains(t, simplePaths.string(), "src-5 = str1-5 to dst-5 = str2-5",
+		"simple path4 not as expected")
+	fmt.Printf("negate path[0]: %v\n", simplePaths[0].negate().string())
+	require.Equal(t, "src-1 != str1-1 to dst-1 != str2-1", simplePaths[0].negate().string(),
+		"negate simple path0 not as expected")
+	require.Equal(t, "src-5 != str1-5 to dst-5 != str2-5", simplePaths[4].negate().string(),
+		"negate simple path4 not as expected")
+}
+
+func TestSymbolicPaths(t *testing.T) {
 	conjSrc, conjDst, conjEmpty := Conjunction{}, Conjunction{}, Conjunction{}
 	simplePaths := simplePaths{}
 	for i := 1; i <= 10; i++ {
@@ -50,7 +76,7 @@ func TestSymbolicExpr(t *testing.T) {
 		conjDst = *conjDst.add(atomic.negate())
 	}
 	conjSymbolicPath := SymbolicPath{conjSrc, conjDst}
-	fmt.Printf("conjSrc: %v -> conjDst : %v\n", conjSrc.string(), conjDst.string())
+	fmt.Printf("conjSrc: %v to conjDst : %v\n", conjSrc.string(), conjDst.string())
 	fmt.Printf("\nconjSymbolicPath:\n%v", conjSymbolicPath.string())
 	fmt.Printf("\nnegate conjSymbolicPath:\n%v", conjSymbolicPath.negate().string())
 	println("conjEmpty", conjEmpty.string())
