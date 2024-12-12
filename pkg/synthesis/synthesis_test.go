ec4fb2c5136dbe096b60264d09b9ac8f10dafea2
shirim <shirim@il.ibm.com>
2024-12-12 11:15:23 +0200

    testing infrastructure

---
+++ pkg/synthesis/synthesis_test.go
@@ -0,0 +1,40 @@
+package synthesis
+
+import (
+	"fmt"
+	"github.com/np-guard/vmware-analyzer/pkg/collector/data"
+	"github.com/np-guard/vmware-analyzer/pkg/logging"
+	"github.com/np-guard/vmware-analyzer/pkg/model"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+type synthesisTest struct {
+	name   string
+	exData data.Example
+}
+
+var allTests = []synthesisTest{
+	{
+		name:   "ExampleDumb",
+		exData: data.ExampleDumb,
+	},
+}
+
+func (a *synthesisTest) run(t *testing.T) {
+	params := model.OutputParameters{
+		Format: "txt",
+	}
+	rc := data.ExamplesGeneration(a.exData)
+	res, err := model.NSXConnectivityFromResourcesContainer(rc, params)
+	require.Nil(t, err)
+	fmt.Println(res)
+}
+
+func TestSynthesis(t *testing.T) {
+	logging.Init(logging.HighVerbosity)
+	for i := range allTests {
+		test := &allTests[i]
+		test.run(t)
+	}
+}
