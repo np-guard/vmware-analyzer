f44b33912c4844ae969ff6b40e5de5ad6a48d099
shirim <shirim@il.ibm.com>
2024-12-19 08:27:38 +0200

    renaming

--- pkg/synthesis/model.go
+++ pkg/synthesis/model.go
@@ -18,36 +18,35 @@ type AbstractModelSyn struct {
 	tags     Tags // todo: should be computed by the collector or here?
 	vms      VMs
 	atomics  symbolicexpr.Atomics // todo: should be used and maintained by FwRule
-	rules    []*symbolicRules     // with default deny
+	policy   []*symbolicPolicy    // with default deny
 }
 
 // Tags map from tag's name to the tag
 type Tags map[string]*collector.Tag
 
-// SymbolicRule input to synthesis. Synthesis very likely to non-prioritized only allow rules
+// symbolicRule input to synthesis. Synthesis very likely to non-prioritized only allow policy
 //
 //nolint:all
-type SymbolicRule struct { // original rule
+type symbolicRule struct { // original rule
 	origRule *dfw.FwRule // original rule
 	// category; for reference, e.g. in the labels or documentation of the synthesized objects
 	// a pass rule is interpreted as deny for the current category
 	origRuleCategory  dfw.DfwCategory
 	origSymbolicPaths *symbolicexpr.SymbolicPaths // symbolic presentation paths defined by the original rule
 	// The following refers to conversion of original allow rule to symbolic paths, as follows:
-	// Assuming there are only allow (non-prioritized, of course) rules.
-	// This is relevant only for allow rules (nil otherwise)
+	// Assuming there are only allow (non-prioritized, of course) policy.
+	// This is relevant only for allow policy (nil otherwise)
 	allowOnlyRulePaths      symbolicexpr.SymbolicPaths
-	allowOnlyEffectingRules []*dfw.FwRule // rules effecting allowOnlyRulePaths (potentially higher priority pass and deny)
-	// Assuming there are prioritized allow and deny rules (but no categories and pass)
-	// This is relevant for allow and deny rules (pass nil), priorities are the same as of the original rules
+	allowOnlyEffectingRules []*dfw.FwRule // policy effecting allowOnlyRulePaths (potentially higher priority pass and deny)
+	// Assuming there are prioritized allow and deny policy (but no categories and pass)
+	// This is relevant for allow and deny policy (pass nil), priorities are the same as of the original policy
 	allowAndDenyRulesPaths     symbolicexpr.SymbolicPaths
-	allowAndDenyEffectingRules []*dfw.FwRule // rules effecting allowAndDenyRulesPaths (potentially higher priority pass)
+	allowAndDenyEffectingRules []*dfw.FwRule // policy effecting allowAndDenyRulesPaths (potentially higher priority pass)
 }
 
-//nolint:all // todo: tmp for defs without implementation
-type symbolicRules struct {
-	inbound  []*SymbolicRule // ordered list inbound SymbolicRule
-	outbound []*SymbolicRule // ordered list outbound SymbolicRule
+type symbolicPolicy struct {
+	inbound  []*symbolicRule // ordered list inbound symbolicRule
+	outbound []*symbolicRule // ordered list outbound symbolicRule
 }
 
 // maps used by AbstractModelSyn
@@ -58,12 +57,12 @@ type Segments map[string]*collector.Segment
 // VMs map from VM name to the VM
 type VMs map[string]*endpoints.VM
 
-func (allRules symbolicRules) string() string {
-	return "\nsymbolicInbound Rules:\n~~~~~~~~~~~~~~~~~~~~~~~\n" + strSymbolicRules(allRules.inbound) +
-		"\nsymbolicOutbound Rules:\n~~~~~~~~~~~~~~~~~~~~~~~~~\n" + strSymbolicRules(allRules.outbound)
+func (policy symbolicPolicy) string() string {
+	return "\nsymbolicInbound Rules:\n~~~~~~~~~~~~~~~~~~~~~~~\n" + strSymbolicRules(policy.inbound) +
+		"\nsymbolicOutbound Rules:\n~~~~~~~~~~~~~~~~~~~~~~~~~\n" + strSymbolicRules(policy.outbound)
 }
 
-func strSymbolicRules(rules []*SymbolicRule) string {
+func strSymbolicRules(rules []*symbolicRule) string {
 	resStr := make([]string, len(rules))
 	for i, rule := range rules {
 		resStr[i] = fmt.Sprintf("\tcategory: %v action: %v paths: %v", rule.origRuleCategory, rule.origRule.Action,
--- pkg/synthesis/synthesis.go
+++ pkg/synthesis/synthesis.go
@@ -15,14 +15,14 @@ func NSXSynthesis(recourses *collector.ResourcesContainerModel, params model.Out
 		return "", err
 	}
 	config := parser.GetConfig()
-	symbolicRules := symbolicRules{}
-	symbolicRules.inbound, symbolicRules.outbound = preProcessing(config.Fw.CategoriesSpecs)
-	fmt.Println(symbolicRules.string())
+	policy := symbolicPolicy{}
+	policy.inbound, policy.outbound = preProcessing(config.Fw.CategoriesSpecs)
+	fmt.Println(policy.string())
 	return "", nil
 }
 
-// preProcessing: convert rules from spec to symbolicRules struct
-func preProcessing(categoriesSpecs []*dfw.CategorySpec) (inbound, outbound []*SymbolicRule) {
+// preProcessing: convert policy from spec to symbolicPolicy struct
+func preProcessing(categoriesSpecs []*dfw.CategorySpec) (inbound, outbound []*symbolicRule) {
 	for _, category := range categoriesSpecs {
 		if len(category.ProcessedRules.Outbound)+len(category.ProcessedRules.Inbound) == 0 {
 			continue
@@ -33,11 +33,11 @@ func preProcessing(categoriesSpecs []*dfw.CategorySpec) (inbound, outbound []*Sy
 	return inbound, outbound
 }
 
-func convertRulesToSymbolicPaths(rules []*dfw.FwRule, category dfw.DfwCategory) []*SymbolicRule {
-	res := make([]*SymbolicRule, len(rules))
+func convertRulesToSymbolicPaths(rules []*dfw.FwRule, category dfw.DfwCategory) []*symbolicRule {
+	res := make([]*symbolicRule, len(rules))
 	for i, rule := range rules {
 		ruleSymbolicPaths := symbolicexpr.ConvertFWRuleToSymbolicPaths(rule)
-		res[i] = &SymbolicRule{origRule: rule, origRuleCategory: category, origSymbolicPaths: ruleSymbolicPaths}
+		res[i] = &symbolicRule{origRule: rule, origRuleCategory: category, origSymbolicPaths: ruleSymbolicPaths}
 	}
 	return res
 }
