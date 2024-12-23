7f033c1dfb87e80e3bbac8cf059be4eca41ad6f0
shirim <shirim@il.ibm.com>
2024-12-23 15:51:22 +0200

    enriched test

--- pkg/symbolicexpr/symbolicexpr_test.go
+++ pkg/symbolicexpr/symbolicexpr_test.go
@@ -261,21 +261,21 @@ func TestComputeAllowGivenDenyDenyTautology(t *testing.T) {
 
 // Input:
 // allow symbolic path:
-// src: (tag = t0) dst: (tag = t1)
+// src: (tag = t0) dst: (tag = t1) TCP
 // src: (tag = t2) dst: (tag = t3)
 // deny symbolic path:
 // src: (segment = s0) dst: (segment = s1)
 // src: (segment = s2) dst: (segment = s3)
 // src: (segment = s4) dst: (segment = s5)
 // Output allow paths:
-// src: (tag = t0 and segment != s0 and segment != s2 and segment != s4) dst: (tag = t1)
-// src: (tag = t0 and segment != s0 and segment != s2) dst: (tag = t1 and segment != s5)
-// src: (tag = t0 and segment != s0 and segment != s4) dst: (tag = t1 and segment != s3)
-// src: (tag = t0 and segment != s0) dst: (tag = t1 and segment != s3 and segment != s5)
-// src: (tag = t0 and segment != s2 and segment != s4) dst: (tag = t1 and segment != s1)
-// src: (tag = t0 and segment != s2) dst: (tag = t1 and segment != s1 and segment != s5)
-// src: (tag = t0 and segment != s4) dst: (tag = t1 and segment != s1 and segment != s3)
-// src: (tag = t0) dst: (tag = t1 and segment != s1 and segment != s3 and segment != s5)
+// src: (tag = t0 and segment != s0 and segment != s2 and segment != s4) dst: (tag = t1) TCP
+// src: (tag = t0 and segment != s0 and segment != s2) dst: (tag = t1 and segment != s5) TCP
+// src: (tag = t0 and segment != s0 and segment != s4) dst: (tag = t1 and segment != s3) TCP
+// src: (tag = t0 and segment != s0) dst: (tag = t1 and segment != s3 and segment != s5) TCP
+// src: (tag = t0 and segment != s2 and segment != s4) dst: (tag = t1 and segment != s1) TCP
+// src: (tag = t0 and segment != s2) dst: (tag = t1 and segment != s1 and segment != s5) TCP
+// src: (tag = t0 and segment != s4) dst: (tag = t1 and segment != s1 and segment != s3) TCP
+// src: (tag = t0) dst: (tag = t1 and segment != s1 and segment != s3 and segment != s5) TCP
 // src: (tag = t2 and segment != s0 and segment != s2 and segment != s4) dst: (tag = t3)
 // src: (tag = t2 and segment != s0 and segment != s2) dst: (tag = t3 and segment != s5)
 // src: (tag = t2 and segment != s0 and segment != s4) dst: (tag = t3 and segment != s3)
@@ -294,7 +294,7 @@ func TestComputeAllowGivenDenies(t *testing.T) {
 			atomicAllowDst := &atomicTerm{property: testTag, toVal: fmt.Sprintf("t%v", 2*i+1)}
 			conjAllowSrc := Conjunction{atomicAllowSrc}
 			conjAllowDst := Conjunction{atomicAllowDst}
-			allowPaths = append(allowPaths, &SymbolicPath{Src: conjAllowSrc, Dst: conjAllowDst, Conn: netset.AllTransports()})
+			allowPaths = append(allowPaths, &SymbolicPath{Src: conjAllowSrc, Dst: conjAllowDst, Conn: netset.AllTCPTransport()})
 		}
 		atomicDenySrc := &atomicTerm{property: testSegment, toVal: fmt.Sprintf("s%v", 2*i)}
 		atomicDenyDst := &atomicTerm{property: testSegment, toVal: fmt.Sprintf("s%v", 2*i+1)}
@@ -306,22 +306,22 @@ func TestComputeAllowGivenDenies(t *testing.T) {
 	res := ComputeAllowGivenDenies(&allowPaths, &denyPaths)
 	fmt.Printf("ComputeAllowGivenDenies:\n%v\n", res.String())
 	require.Equal(t,
-		"All Connections from (tag = t0 and segment != s0 and segment != s2 and segment != s4) to (tag = t1)\n"+
-			"All Connections from (tag = t0 and segment != s0 and segment != s2) to (tag = t1 and segment != s5)\n"+
-			"All Connections from (tag = t0 and segment != s0 and segment != s4) to (tag = t1 and segment != s3)\n"+
-			"All Connections from (tag = t0 and segment != s0) to (tag = t1 and segment != s3 and segment != s5)\n"+
-			"All Connections from (tag = t0 and segment != s2 and segment != s4) to (tag = t1 and segment != s1)\n"+
-			"All Connections from (tag = t0 and segment != s2) to (tag = t1 and segment != s1 and segment != s5)\n"+
-			"All Connections from (tag = t0 and segment != s4) to (tag = t1 and segment != s1 and segment != s3)\n"+
-			"All Connections from (tag = t0) to (tag = t1 and segment != s1 and segment != s3 and segment != s5)\n"+
-			"All Connections from (tag = t2 and segment != s0 and segment != s2 and segment != s4) to (tag = t3)\n"+
-			"All Connections from (tag = t2 and segment != s0 and segment != s2) to (tag = t3 and segment != s5)\n"+
-			"All Connections from (tag = t2 and segment != s0 and segment != s4) to (tag = t3 and segment != s3)\n"+
-			"All Connections from (tag = t2 and segment != s0) to (tag = t3 and segment != s3 and segment != s5)\n"+
-			"All Connections from (tag = t2 and segment != s2 and segment != s4) to (tag = t3 and segment != s1)\n"+
-			"All Connections from (tag = t2 and segment != s2) to (tag = t3 and segment != s1 and segment != s5)\n"+
-			"All Connections from (tag = t2 and segment != s4) to (tag = t3 and segment != s1 and segment != s3)\n"+
-			"All Connections from (tag = t2) to (tag = t3 and segment != s1 and segment != s3 and segment != s5)",
+		"TCP from (tag = t0 and segment != s0 and segment != s2 and segment != s4) to (tag = t1)\n"+
+			"TCP from (tag = t0 and segment != s0 and segment != s2) to (tag = t1 and segment != s5)\n"+
+			"TCP from (tag = t0 and segment != s0 and segment != s4) to (tag = t1 and segment != s3)\n"+
+			"TCP from (tag = t0 and segment != s0) to (tag = t1 and segment != s3 and segment != s5)\n"+
+			"TCP from (tag = t0 and segment != s2 and segment != s4) to (tag = t1 and segment != s1)\n"+
+			"TCP from (tag = t0 and segment != s2) to (tag = t1 and segment != s1 and segment != s5)\n"+
+			"TCP from (tag = t0 and segment != s4) to (tag = t1 and segment != s1 and segment != s3)\n"+
+			"TCP from (tag = t0) to (tag = t1 and segment != s1 and segment != s3 and segment != s5)\n"+
+			"TCP from (tag = t2 and segment != s0 and segment != s2 and segment != s4) to (tag = t3)\n"+
+			"TCP from (tag = t2 and segment != s0 and segment != s2) to (tag = t3 and segment != s5)\n"+
+			"TCP from (tag = t2 and segment != s0 and segment != s4) to (tag = t3 and segment != s3)\n"+
+			"TCP from (tag = t2 and segment != s0) to (tag = t3 and segment != s3 and segment != s5)\n"+
+			"TCP from (tag = t2 and segment != s2 and segment != s4) to (tag = t3 and segment != s1)\n"+
+			"TCP from (tag = t2 and segment != s2) to (tag = t3 and segment != s1 and segment != s5)\n"+
+			"TCP from (tag = t2 and segment != s4) to (tag = t3 and segment != s1 and segment != s3)\n"+
+			"TCP from (tag = t2) to (tag = t3 and segment != s1 and segment != s3 and segment != s5)",
 		ComputeAllowGivenDenies(&allowPaths, &denyPaths).String(),
 		"ComputeAllowGivenDenies computation not as expected")
 }
