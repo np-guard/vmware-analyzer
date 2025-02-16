ee6e4f869c13edd497f5eda9ad30355cd38ea372
shirim <shirim@il.ibm.com>
2025-02-16 12:21:49 +0200

    removed redundant var

--- pkg/synthesis/model.go
+++ pkg/synthesis/model.go
@@ -42,7 +42,6 @@ type symbolicRule struct { // original rule
 	// This is relevant only for allow policy (nil otherwise)
 	// and only for categories greater than allowOnlyFromCategory
 	allowOnlyRulePaths symbolicexpr.SymbolicPaths
-	pathsToSynthesis   *symbolicexpr.SymbolicPaths
 }
 
 type symbolicPolicy struct {
