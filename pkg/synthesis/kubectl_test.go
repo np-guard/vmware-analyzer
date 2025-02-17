71078f5e35e5fd9ab63542f647dfbc76d9719bcb
haim kermany <haim.kermany@gmail.com>
2025-02-17 04:32:00 -0600

    runK8STraceFlow()

--- pkg/synthesis/kubectl_test.go
+++ pkg/synthesis/kubectl_test.go
@@ -6,8 +6,12 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/np-guard/vmware-analyzer/pkg/collector"
 	"github.com/np-guard/vmware-analyzer/pkg/common"
+	"github.com/np-guard/vmware-analyzer/pkg/logging"
 	"github.com/stretchr/testify/require"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 func TestKubeCTL(t *testing.T) {
@@ -18,22 +22,79 @@ func TestKubeCTL(t *testing.T) {
 	ctl.waitPod("pod-a")
 	ctl.waitPod("pod-b")
 	ctl.testPodsConnection("pod-a", "pod-b", 1)
-	ctl.exposePod("pod-b", []string{"group__gb=true"})
+	ctl.exposePod("pod-b")
 	ctl.testPodsConnection("pod-a", "pod-b", 0)
 	require.Nil(t, ctl.createDefaultDeny())
 	ctl.testPodsConnection("pod-a", "pod-b", 1)
 	require.Nil(t, ctl.createDNSPolicy())
 	ctl.testPodsConnection("pod-a", "pod-b", 1)
-	require.Nil(t, ctl.createAllowPolicy("pod-a", "pod-b","group__ga","group__gb",  false ))
+	require.Nil(t, ctl.createAllowPolicy("pod-a", "pod-b", "group__ga", "group__gb", false))
 	ctl.testPodsConnection("pod-a", "pod-b", 1)
-	require.Nil(t, ctl.createAllowPolicy("pod-b", "pod-a","group__gb","group__ga",  true ))
+	require.Nil(t, ctl.createAllowPolicy("pod-b", "pod-a", "group__gb", "group__ga", true))
 	ctl.testPodsConnection("pod-a", "pod-b", 0)
 
 	ctl.deletePod("pod-a")
 	ctl.deletePod("pod-b")
 	ctl.clean()
-	require.Nil(t, ctl.createCmdFile())
+	require.Nil(t, ctl.createCmdFile(path.Join(getTestsDirActualOut(), "kube_tests", "cmd.sh")))
 
+}
+
+func runK8STraceFlow(synTest *synthesisTest, t *testing.T, rc *collector.ResourcesContainerModel) {
+	err := logging.Tee(path.Join(synTest.debugDir(), "runK8STraceFlow.log"))
+	require.Nil(t, err)
+	kubeDir := path.Join(synTest.debugDir(), "kube_test_dir")
+	k8sDir := path.Join(kubeDir, k8sResourcesDir)
+	// create K8S resources:
+	resources, err := NSXToK8sSynthesis(rc, synTest.hints(), synTest.synthesizeAdmin, false)
+	require.Nil(t, err)
+	for i := range resources.pods {
+		name := strings.ToLower(resources.pods[i].Name)
+		resources.pods[i].Spec.Containers = []core.Container{{Name: "app-on-two-ports", Image: "ahmet/app-on-two-ports"}}
+		resources.pods[i].Name = name
+	}
+	port := intstr.FromInt(5000)
+	for in := range resources.networkPolicies {
+		for ie := range resources.networkPolicies[in].Spec.Egress {
+			for ip := range resources.networkPolicies[in].Spec.Egress[ie].Ports {
+				if *resources.networkPolicies[in].Spec.Egress[ie].Ports[ip].Protocol == core.ProtocolTCP {
+					resources.networkPolicies[in].Spec.Egress[ie].Ports[ip].Port = &port
+				}
+			}
+		}
+		for ie := range resources.networkPolicies[in].Spec.Ingress {
+			for ip := range resources.networkPolicies[in].Spec.Ingress[ie].Ports {
+				if *resources.networkPolicies[in].Spec.Ingress[ie].Ports[ip].Protocol == core.ProtocolTCP {
+					resources.networkPolicies[in].Spec.Ingress[ie].Ports[ip].Port = &port
+				}
+			}
+		}
+	}
+	err = resources.CreateDir(kubeDir)
+	require.Nil(t, err)
+
+	ctl := kubeCTL{}
+	ctl.clean()
+	ctl.addCmd("kubectl apply -f " + path.Join(k8sDir, "pods.yaml"))
+	for i := range resources.pods {
+		ctl.exposePod(resources.pods[i].Name)
+	}
+	ctl.addCmd("kubectl apply -f " + path.Join(k8sDir, "policies.yaml"))
+	for i := range resources.pods {
+		ctl.waitPod(resources.pods[i].Name)
+	}
+	for i := range resources.pods {
+		for j := range resources.pods {
+			if i != j {
+				ctl.testPodsConnection(resources.pods[i].Name, resources.pods[j].Name, 0)
+			}
+		}
+	}
+
+	for i := range resources.pods {
+		ctl.deletePod(resources.pods[i].Name)
+	}
+	require.Nil(t, ctl.createCmdFile(path.Join(kubeDir, "cmd.sh")))
 }
 
 type kubeCTL struct {
@@ -52,10 +113,8 @@ func (ctl *kubeCTL) createPod(name string, labels []string) {
 	labelsStr := strings.Join(labels, ",")
 	ctl.addCmd(fmt.Sprintf("kubectl run %s --image=ahmet/app-on-two-ports --labels=\"%s\"", name, labelsStr))
 }
-func (ctl *kubeCTL) exposePod(name string, labels []string) {
-	labelsStr := strings.Join(labels, ",")
-	ctl.addCmd(fmt.Sprintf("kubectl expose pod %s --port=5001 --target-port=5000 --selector=\"%s\" --name \"%s-service\"", name, labelsStr, name))
-	ctl.addCmd("sleep 3")
+func (ctl *kubeCTL) exposePod(name string) {
+	ctl.addCmd(fmt.Sprintf("kubectl expose pod %s --port=5001 --target-port=5000 --name \"%s-service\"", name, name))
 }
 
 func (ctl *kubeCTL) waitPod(name string) {
@@ -66,11 +125,11 @@ func (ctl *kubeCTL) deletePod(name string) {
 }
 func (ctl *kubeCTL) testPodsConnection(from, to string, er int) {
 	ctl.addCmd(fmt.Sprintf("kubectl exec %s -- wget -qO- --timeout=2 http://%s-service:5001/metrics", from, to))
-	ctl.addCmd(fmt.Sprintf("echo Connection Test exit status should be %d: exit status = $?", er))
+	ctl.addCmd(fmt.Sprintf("echo Connection Test from %s to %s, exit status should be %d: exit status = $?", from, to, er))
 }
 
-func (ctl *kubeCTL) createCmdFile() error {
-	return common.WriteToFile(path.Join(getTestsDirActualOut(), "kube_tests", "cmd.sh"), strings.Join(ctl.cmdLines, "\n"))
+func (ctl *kubeCTL) createCmdFile(fileName string) error {
+	return common.WriteToFile(fileName, strings.Join(ctl.cmdLines, "\n"))
 }
 
 func (ctl *kubeCTL) createDefaultDeny() error {
