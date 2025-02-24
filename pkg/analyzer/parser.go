57f88440b019cbe3ab1f1d9879bfbe7ffa575b6e
shirim <shirim@il.ibm.com>
2025-02-24 09:32:59 +0200

    fix spelling mistake

--- pkg/analyzer/parser.go
+++ pkg/analyzer/parser.go
@@ -351,7 +351,7 @@ func (p *NSXConfigParser) getRuleConnections(rule *collector.Rule) *netset.Trans
 	}
 	res := netset.NoTransports()
 	for _, s := range rule.Services {
-		// currenrly ignoring services "ANY", if ServiceEntries is not empty..
+		// currently, ignoring services "ANY", if ServiceEntries is not empty..
 		if s == anyStr {
 			logging.Debugf("warning: for rule %d, found rule.Services containing ANY, with non empty serviceEntries. ignoring ANY for this rule.",
 				*rule.RuleId)
