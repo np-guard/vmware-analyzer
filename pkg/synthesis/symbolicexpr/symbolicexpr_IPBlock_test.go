41ec110c27d0f59e1da2492999ca5f083b2bd6e6
shirim <shirim@il.ibm.com>
2025-03-04 12:02:26 +0200

    check negation (1)

--- pkg/synthesis/symbolicexpr/symbolicexpr_IPBlock_test.go
+++ pkg/synthesis/symbolicexpr/symbolicexpr_IPBlock_test.go
@@ -19,14 +19,22 @@ func TestIpBlockTerm(t *testing.T) {
 	ipBlockTerm1 := NewIPBlockTermTerm(&topology.IpBlock{Block: ipBlock1, OriginalIP: "1.2.3.0/8"})
 	ipBlockTerm2 := NewIPBlockTermTerm(&topology.IpBlock{Block: ipBlock2, OriginalIP: "1.2.3.0/16"})
 	ipBlockTerm3 := NewIPBlockTermTerm(&topology.IpBlock{Block: ipBlock3, OriginalIP: "192.0.2.0/24"})
-	ipAddrSingleTerm := NewIPBlockTermTerm(&topology.IpBlock{Block: ipAddrSingle, OriginalIP: "192.0.2.0"})
+	ipAddrSingleTerm := NewIPBlockTermTerm(&topology.IpBlock{Block: ipAddrSingle, OriginalIP: "192.0.2.0 originalIP"})
 	fmt.Println("allIpBlockTerm is", allIpBlockTerm)
 	fmt.Println("ipBlockTerm1 is", ipBlockTerm1)
 	fmt.Println("ipBlockTerm2 is", ipBlockTerm2)
 	fmt.Println("ipBlockTerm3 is", ipBlockTerm3)
 	fmt.Println("ipAddrSingleTerm is", ipAddrSingleTerm)
+
 	// checks String and Name
 	require.Equal(t, "IP block in 0.0.0.0/0", allIpBlockTerm.String())
 	require.Equal(t, "IP block in 192.0.2.0/24", ipBlockTerm3.String())
-	require.Equal(t, "IP block in 192.0.2.0", ipAddrSingleTerm.String())
+	require.Equal(t, "IP block in 192.0.2.0 originalIP", ipAddrSingleTerm.String())
+
+	// checks negation
+	fmt.Println("neg ipBlockTerm3 is", ipBlockTerm3.negate())
+	fmt.Println("neg ipAddrSingleTerm is", ipAddrSingleTerm.negate())
+	require.Equal(t, "IP block not in 192.0.2.0/24", ipBlockTerm3.negate().String())
+	require.Equal(t, "IP block not in 192.0.2.0 originalIP", ipAddrSingleTerm.negate().String())
+
 }
