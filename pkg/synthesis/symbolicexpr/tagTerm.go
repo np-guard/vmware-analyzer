30a8d0f1346f0d71982e985c31d14626ed334404
haim <haim.kermany@gmail.com>
2025-03-12 09:41:45 +0200

    fix pull from main

--- pkg/synthesis/symbolicexpr/tagTerm.go
+++ pkg/synthesis/symbolicexpr/tagTerm.go
@@ -51,7 +51,7 @@ func (tagTerm tagAtomicTerm) isNegateOf(otherAtom atomic) bool {
 
 // returns true iff otherAt is disjoint to otherAtom as given by hints
 func (tagTerm tagAtomicTerm) disjoint(otherAtom atomic, hints *Hints) bool {
-	if otherAtom.getBlock() != nil {
+	if otherAtom.GetBlock() != nil {
 		return true // otherAtom is an IPBlock; external IP block is disjoint to tag terms referring to VMs
 	}
 	return disjoint(tagTerm, otherAtom, hints)
