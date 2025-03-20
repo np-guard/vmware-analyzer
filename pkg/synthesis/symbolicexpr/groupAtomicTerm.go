2c5de686c8510f9fc7c4cf4f7e550f0975470e27
shirim <shirim@il.ibm.com>
2025-03-20 13:39:34 +0200

    getConjunctionForGroups relevant for all groups

--- pkg/synthesis/symbolicexpr/groupAtomicTerm.go
+++ pkg/synthesis/symbolicexpr/groupAtomicTerm.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 
 	"github.com/np-guard/vmware-analyzer/pkg/collector"
-	"github.com/np-guard/vmware-analyzer/pkg/logging"
 )
 
 const grp = "group"
@@ -29,40 +28,6 @@ func NewGroupAtomicTerm(group *collector.Group, neg bool) *groupAtomicTerm {
 func (groupTerm groupAtomicTerm) negate() atomic {
 	return groupAtomicTerm{abstractGroupTerm: abstractGroupTerm{group: groupTerm.abstractGroupTerm.group},
 		atomicTerm: atomicTerm{neg: !groupTerm.neg}}
-}
-
-// Evaluates group and translates it into []*Conjunction
-// If group has no expr or evaluation expr fails then uses the group names in  Conjunction
-func getConjunctionForGroups(groups []*collector.Group, groupToConjunctions map[string][]*Conjunction,
-	ruleID int) []*Conjunction {
-	res := []*Conjunction{}
-	for _, group := range groups {
-		// todo: treat negation properly
-		if cachedGroupConj, ok := groupToConjunctions[group.Name()]; ok {
-			res = append(res, cachedGroupConj...)
-			continue
-		}
-		// not in cache
-		// default: Conjunction defined via group only
-		groupConj := []*Conjunction{{groupAtomicTerm{abstractGroupTerm: abstractGroupTerm{group: group},
-			atomicTerm: atomicTerm{neg: false}}}}
-		synthesisUseGroup := fmt.Sprintf("group %s, referenced by FW rule with ID %d, "+
-			"synthesis will be based only on its name", group.Name(), ruleID)
-		// if group has a tag based supported expression then considers the tags
-		if len(group.Expression) > 0 {
-			tagConj := GetTagConjunctionForExpr(&group.Expression, group.Name())
-			if tagConj != nil {
-				groupConj = tagConj
-			} else {
-				logging.Debugf("for %s", synthesisUseGroup)
-			}
-		} else {
-			logging.Debugf("No expression is attached to %s", synthesisUseGroup)
-		}
-		groupToConjunctions[group.Name()] = groupConj
-		res = append(res, groupConj...)
-	}
-	return res
 }
 
 // returns true iff otherAtom is negation of groupTerm
