1861e2b492d168e16ed3200ef33dec31a218e113
shirim <shirim@il.ibm.com>
2025-03-26 16:50:27 +0200

    bug fix - should be proper superset

--- pkg/synthesis/symbolicexpr/internalIPTerm.go
+++ pkg/synthesis/symbolicexpr/internalIPTerm.go
@@ -55,8 +55,9 @@ func (internalIP internalIPTerm) disjoint(otherAtom atomic, hints *Hints) bool {
 func (internalIP internalIPTerm) supersetOf(otherAtom atomic, hints *Hints) bool {
 	// if otherAtom is also internal Block, then check explicit containment
 	otherInternalBlock := otherAtom.getInternalBlock()
-	if otherInternalBlock != nil {
-		if otherInternalBlock.Intersect(internalIP.getInternalBlock().Complementary()).IsEmpty() {
+	internalBlock := internalIP.getInternalBlock()
+	if otherInternalBlock != nil && !otherInternalBlock.Equal(internalBlock) {
+		if otherInternalBlock.Intersect(internalBlock.Complementary()).IsEmpty() {
 			return true
 		}
 	}
