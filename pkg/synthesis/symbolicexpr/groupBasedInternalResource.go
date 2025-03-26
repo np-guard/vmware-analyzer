804eacfa4ca1da03e723d54b7d4ac823820666fe
shirim <shirim@il.ibm.com>
2025-03-26 11:18:08 +0200

    lint

--- pkg/synthesis/symbolicexpr/groupBasedInternalResource.go
+++ pkg/synthesis/symbolicexpr/groupBasedInternalResource.go
@@ -2,6 +2,7 @@ package symbolicexpr
 
 import (
 	"fmt"
+
 	"github.com/np-guard/models/pkg/netset"
 	"github.com/np-guard/vmware-analyzer/pkg/collector"
 	"github.com/np-guard/vmware-analyzer/pkg/logging"
