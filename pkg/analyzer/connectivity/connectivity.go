214b9e75e8959bfd38b4bcef7c6c9e7f2633a7f5
haim <haim.kermany@gmail.com>
2025-04-01 17:22:16 +0300

    typo

--- pkg/analyzer/connectivity/connectivity.go
+++ pkg/analyzer/connectivity/connectivity.go
@@ -80,8 +80,8 @@ func (c ConnMap) getEntryPerEndpoints(srcVM, dstVM string) *connMapEntry {
 	return nil
 }
 
-// this method is for testing only. grouping the external, to compare connectivities
-// assuming there is no connection with both src and dst external
+// this method is for testing only. grouping the external EPs, to compare maps
+// assuming there is no connection with both external src and dst
 func (c ConnMap) GroupExternalEP() ConnMap {
 	unionExternalEP := func(e1, e2 topology.Endpoint) topology.Endpoint {
 		return topology.NewExternalIP(
