d8ad0e602554f9a53e5fa5287a3f5054e9be4596
haim kermany <haim.kermany@gmail.com>
2025-04-02 08:59:56 -0500

    clean filter

--- pkg/analyzer/verify_trace_flow_test.go
+++ pkg/analyzer/verify_trace_flow_test.go
@@ -53,7 +53,8 @@ func Test_verifyTraceflow(t *testing.T) {
 				t.Errorf(common.ErrNoResources)
 				return
 			}
-			filter := func(vm topology.Endpoint) bool { return strings.Contains(vm.Name(), "New-VM-1") || strings.Contains(vm.Name(), "New-VM-2")}
+			filter := func(vm topology.Endpoint) bool { return strings.Contains(vm.Name(), "")}
+			// filter := func(vm topology.Endpoint) bool { return strings.Contains(vm.Name(), "New-VM-1") || strings.Contains(vm.Name(), "New-VM-2")}
 			tfs, err := compareConfigToTraceflows(collectedResources, server, filter)
 			if err != nil {
 				t.Errorf("verifyTraceflow() error = %v", err)
