7426ff0feef36a46b8a3505770626595622232b6
haim kermany <haim.kermany@gmail.com>
2025-04-03 00:08:35 -0500

    siplify code

--- pkg/analyzer/verify_trace_flow_test.go
+++ pkg/analyzer/verify_trace_flow_test.go
@@ -20,55 +20,42 @@ const (
 )
 
 func Test_verifyTraceflow(t *testing.T) {
-	type args struct {
+	args := struct {
 		nsxServer          string
 		userName, password string
-	}
-	tests := []struct {
-		name string
-		args args
 	}{
-		{
-			"simple",
-			args{
-				// you can set your server info here
-			},
-		},
+		// you can set your server info here
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			require.Nil(t, logging.Init(logging.HighVerbosity, path.Join(outDir, "traceflows.log")))
-			server, err := collector.GetNSXServerDate(tt.args.nsxServer, tt.args.userName, tt.args.password, true)
-			if err != nil {
-				// do not fail on env without access to nsx host
-				fmt.Println(err.Error())
-				return
-			}
-			collectedResources, err := collector.CollectResources(server)
-			if err != nil {
-				t.Errorf("CollectResources() error = %v", err)
-				return
-			}
-			if collectedResources == nil {
-				t.Errorf(common.ErrNoResources)
-				return
-			}
-			filter := func(vm topology.Endpoint) bool { return strings.Contains(vm.Name(), "")}
-			// filter := func(vm topology.Endpoint) bool { return strings.Contains(vm.Name(), "New-VM-1") || strings.Contains(vm.Name(), "New-VM-2")}
-			tfs, err := compareConfigToTraceflows(collectedResources, server, filter)
-			if err != nil {
-				t.Errorf("verifyTraceflow() error = %v", err)
-			}
-			jOut, err := tfs.ToJSONString()
-			if err != nil {
-				t.Errorf("ToJSONString() error = %v", err)
-			}
-			tfPath := path.Join(outDir, "traceflowsObservations.json")
-			if err := common.WriteToFile(tfPath, jOut); err != nil {
-				t.Errorf("ToJSONString() error = %v", err)
-			}
-			fmt.Printf("traceflow results at %s\n", tfPath)
-		})
+	require.Nil(t, logging.Init(logging.HighVerbosity, path.Join(outDir, "traceflows.log")))
+	server, err := collector.GetNSXServerDate(args.nsxServer, args.userName, args.password, true)
+	if err != nil {
+		// do not fail on env without access to nsx host
+		fmt.Println(err.Error())
+		return
 	}
+	collectedResources, err := collector.CollectResources(server)
+	if err != nil {
+		t.Errorf("CollectResources() error = %v", err)
+		return
+	}
+	if collectedResources == nil {
+		t.Errorf(common.ErrNoResources)
+		return
+	}
+	filter := func(vm topology.Endpoint) bool { return strings.Contains(vm.Name(), "") }
+	// filter := func(vm topology.Endpoint) bool { return strings.Contains(vm.Name(), "New-VM-1") || strings.Contains(vm.Name(), "New-VM-2")}
+	tfs, err := compareConfigToTraceflows(collectedResources, server, filter)
+	if err != nil {
+		t.Errorf("verifyTraceflow() error = %v", err)
+	}
+	jOut, err := tfs.ToJSONString()
+	if err != nil {
+		t.Errorf("ToJSONString() error = %v", err)
+	}
+	tfPath := path.Join(outDir, "traceflowsObservations.json")
+	if err := common.WriteToFile(tfPath, jOut); err != nil {
+		t.Errorf("ToJSONString() error = %v", err)
+	}
+	fmt.Printf("traceflow results at %s\n", tfPath)
 	fmt.Printf("done")
 }
