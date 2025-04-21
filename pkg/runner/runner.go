2481dba0906823774d1dc88520d72c25316d1414
adisos <adisos@il.ibm.com>
2025-04-21 08:38:42 +0300

    fix merge commit
    Signed-off-by: adisos <adisos@il.ibm.com>

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -405,14 +405,8 @@ func WithInsecureSkipVerify(insecureSkipVerify bool) RunnerOption {
 	}
 }
 
-<<<<<<< HEAD
-func WithLint(doLint bool) RunnerOption {
-	return func(r *Runner) {
-		r.lint = doLint
-=======
 func WithLint(lintCmd bool) RunnerOption {
 	return func(r *Runner) {
 		r.lint = lintCmd
->>>>>>> main
 	}
 }
